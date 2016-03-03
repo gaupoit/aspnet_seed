43755ff6fca8e87158b79a909f40d56954b046df
Thinh Nguyen <phuoc@niometrics.com>
2016-03-03 17:49:21 +0800

    init live reload

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,30 @@
 //******************************************************************************
 //* DEPENDENCIES
 //******************************************************************************
-var gulp        = require("gulp"),
-    browserify  = require("browserify"),
-    source      = require("vinyl-source-stream"),
-    buffer      = require("vinyl-buffer"),
-    tslint      = require("gulp-tslint"),
-    tsc         = require("gulp-typescript"),
-    sourcemaps  = require("gulp-sourcemaps"),
-    uglify      = require("gulp-uglify"),
-    runSequence = require("run-sequence"),
-    mocha       = require("gulp-mocha"),
-    istanbul    = require("gulp-istanbul"),
-    browserSync = require('browser-sync').create();
+const   gulp = require("gulp"),
+        browserify = require("browserify"),
+        source = require("vinyl-source-stream"),
+        buffer = require("vinyl-buffer"),
+        tslint = require("gulp-tslint"),
+        tsc = require("gulp-typescript"),
+        sourcemaps = require("gulp-sourcemaps"),
+        uglify = require("gulp-uglify"),
+        runSequence = require("run-sequence"),
+        mocha = require("gulp-mocha"),
+        istanbul = require("gulp-istanbul"),
+        browserSync = require('browser-sync').create(),
+        appFolder = "wwwroot/app/";
     
 //******************************************************************************
 //* LINT
 //******************************************************************************
-gulp.task("lint", function() {
+gulp.task("lint", function () {
     return gulp.src([
-        "source/**/**.ts",
-        "test/**/**.test.ts"
+        appFolder + "source/**/**.ts",
+        appFolder + "test/**/**.test.ts"
     ])
-    .pipe(tslint({ }))
-    .pipe(tslint.report("verbose"));
+        .pipe(tslint({}))
+        .pipe(tslint.report("verbose"));
 });
 
 //******************************************************************************
@@ -33,64 +34,64 @@ gulp.task("lint", function() {
 //******************************************************************************
 var tsProject = tsc.createProject("tsconfig.json");
 
-gulp.task("build-app", function() {
+gulp.task("build-app", function () {
     return gulp.src([
-            "source/**/**.ts",
-            "typings/main.d.ts/",
-            "source/interfaces/interfaces.d.ts"
-        ])
+        appFolder + "source/**/**.ts",
+        appFolder + "typings/main.d.ts/",
+        appFolder + "source/interfaces/interfaces.d.ts"
+    ])
         .pipe(tsc(tsProject))
         .js.pipe(gulp.dest("source/"));
 });
 
 var tsTestProject = tsc.createProject("tsconfig.json");
 
-gulp.task("build-test", function() {
+gulp.task("build-test", function () {
     return gulp.src([
-            "test/**/*.ts",
-            "typings/main.d.ts/",
-            "source/interfaces/interfaces.d.ts"
-        ])
+        appFolder + "test/**/*.ts",
+        appFolder + "typings/main.d.ts/",
+        appFolder + "source/interfaces/interfaces.d.ts"
+    ])
         .pipe(tsc(tsTestProject))
         .js.pipe(gulp.dest("test/"));
 });
 
-gulp.task("build", function(cb) {
+gulp.task("build", function (cb) {
     runSequence(["build-app", "build-test"], cb);
 });
 
 //******************************************************************************
 //* TEST
 //******************************************************************************
-gulp.task("istanbul:hook", function() {
+gulp.task("istanbul:hook", function () {
     return gulp.src(['source/**/*.js'])
-        // Covering files
+    // Covering files
         .pipe(istanbul())
-        // Force `require` to return covered files
+    // Force `require` to return covered files
         .pipe(istanbul.hookRequire());
 });
 
-gulp.task("test", ["istanbul:hook"], function() {
+gulp.task("test", ["istanbul:hook"], function () {
     return gulp.src('test/**/*.test.js')
-        .pipe(mocha({ui: 'bdd'}))
+        .pipe(mocha({ ui: 'bdd' }))
         .pipe(istanbul.writeReports());
 });
 
 //******************************************************************************
 //* BUNDLE
 //******************************************************************************
-gulp.task("bundle", function() {
-  
-    var libraryName = "myapp";
-    var mainTsFilePath = "source/main.js";
-    var outputFolder   = "dist/";
+gulp.task("bundle", function () {
+
+    var libraryName = "seed_app";
+    var mainTsFilePath = appFolder + "source/main.ts";
+    var outputFolder = appFolder + "dist/";
     var outputFileName = libraryName + ".min.js";
 
     var bundler = browserify({
         debug: true,
-        standalone : libraryName
+        standalone: libraryName
     });
-    
+
     return bundler.add(mainTsFilePath)
         .bundle()
         .pipe(source(outputFileName))
@@ -105,13 +106,11 @@ gulp.task("bundle", function() {
 //* DEV SERVER
 //******************************************************************************
 gulp.task("watch", ["default"], function () {
-    
-    browserSync.init({
-        server: "."
-    });
-    
-    gulp.watch([ "source/**/**.ts", "test/**/*.ts"], ["default"]);
-    gulp.watch("dist/*.js").on('change', browserSync.reload); 
+
+    browserSync.init();
+
+    gulp.watch([appFolder + "source/**/**.ts", appFolder + "test/**/*.ts"], ["default"]);
+    gulp.watch(appFolder + "dist/*.js").on('change', browserSync.reload);
 });
 
 //******************************************************************************
